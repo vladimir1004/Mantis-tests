e78a83584ac7d3c82993ee068cf30eec6cdb0889
Karakis Vladimir <0975059295v@gmail.com>
2017-09-03 21:22:19 +0300

    add new project

---
+++ src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -0,0 +1,53 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+
+public class ApplicationManager {
+  private final Properties properties;
+  WebDriver driver;
+
+
+  private String browser;
+
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+    properties = new Properties();
+  }
+
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/&s.properties", target))));
+
+    if (browser.equals(BrowserType.FIREFOX)) {
+      driver = new FirefoxDriver();
+    } else if (browser.equals(BrowserType.CHROME)) {
+      driver = new ChromeDriver();
+    } else if (browser.equals(BrowserType.EDGE)) {
+      driver = new EdgeDriver();
+    }
+
+    driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    driver.get(properties.getProperty("web.baseUrl"));
+
+  }
+
+
+  public void stop() {
+    driver.quit();
+  }
+
+}
+
